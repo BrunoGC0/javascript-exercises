2596d1de9f0964a15b0b814fd21207d7fd07b66d
Bruno Gonçalves <brunogcp28@gmail.com>
2025-03-24 07:43:44 -0400

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(name, number) {
+    let string = ""
+    for(let i = 1; i <= number; i++){
+        string += name
+    }
 
-};
+    return string
+}
+
+console.log(repeatString('goodbye', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
