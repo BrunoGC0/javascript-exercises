447388a44bd92d4fad5069534595d30ce5e49bab
Bruno Gonçalves <brunogcp28@gmail.com>
2025-03-25 07:58:05 -0400

    Add Solution Activity 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const numbers = [1, 2, 3];
+
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            newArray.push(item)
+        }
+    });
+
+    return newArray;
 };
+
+console.log(removeFromArray(numbers, "1", 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
