55d6c17a534afa27981374af62d43dbc1f169b57
Bruno Gonçalves <brunogcp28@gmail.com>
2025-03-27 06:45:08 -0400

    Add activity 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let start = Math.min(number1, number2);
+    let end = Math.max(number1, number2);
+    let soma = 0;
 
+    for(let i = start; i <= end; i++){
+        soma += i
+    }
+
+    return soma;
 };
+
+console.log(sumAll(2, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
