6d7c7044da5a7c23dde1320fed63c7f12fd2e727
Bruno Gonçalves <brunogcp28@gmail.com>
2025-03-27 07:46:49 -0400

    Add activity 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const anoQuatro = year % 4 === 0;
+    const anoCem = year % 100 === 0;
+    const anoQuatrocentos = year % 400 === 0;
 
+    if(anoQuatro && !anoCem || anoQuatrocentos){
+        return true;
+    }else{
+        return false;
+    }
 };
 
+console.log(leapYears(2000))
 // Do not edit below this line
 module.exports = leapYears;
